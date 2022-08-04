22361807002bc279011ec98fc70b30ca7f777d1f
Emily Jin <emilyjin@stanford.edu>
2022-08-04 13:52:46 -0700

    edit envs/__init__.py

--- gym_minigrid/envs/__init__.py
+++ gym_minigrid/envs/__init__.py
@@ -3,6 +3,5 @@ from gym_minigrid.envs.navigation import *
 from gym_minigrid.envs.floorplan import *
 from gym_minigrid.envs.throwleftovers_floorplan import *
 from gym_minigrid.envs.transition import *
-from gym_minigrid.envs.test import *
-from gym_minigrid.envs.test_inside import *
-from gym_minigrid.envs.test_abilities import *
+# from gym_minigrid.envs.test_inside import *
+# from gym_minigrid.envs.test_abilities import *
