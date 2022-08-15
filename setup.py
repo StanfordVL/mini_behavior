fb6c0906c86fcfa5007569180736d47036a63d51
mjlbach <m.j.lbach@gmail.com>
2022-08-15 10:53:39 -0700

    Fix hardcoded path, missing tests, gitignore (#2)

--- setup.py
+++ setup.py
@@ -11,28 +11,38 @@ with open("README.md") as fh:
         else:
             break
 
+# pytest is pinned to 7.0.1 as this is last version for python 3.6
+extras = {"testing": ["pytest==7.0.1"]}
+
 setup(
-    name='gym_minigrid',
+    name="gym_minigrid",
     author="Farama Foundation",
     author_email="jkterry@farama.org",
-    version='seed 0_2.0.seed 0_2',
-    keywords='memory, environment, agent, rl, gym',
-    url='https://github.com/Farama-Foundation/gym-minigrid',
-    description='Minimalistic gridworld reinforcement learning environments',
-    packages=['gym_minigrid', 'gym_minigrid.envs'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    version="1.1.0",
+    keywords="memory, environment, agent, rl, gym",
+    url="https://github.com/Farama-Foundation/gym-minigrid",
+    description="Minimalistic gridworld reinforcement learning environments",
+    extras_require=extras,
+    packages=["gym_minigrid", "gym_minigrid.envs"],
+    entry_points={
+        "gym.envs": ["__root__ = gym_minigrid.__init__:register_minigrid_envs"]
+    },
+    license="Apache",
     long_description=long_description,
-    python_requires=">=seed 10_3.7, <seed 10_3.11",
     long_description_content_type="text/markdown",
     install_requires=[
-        'gym>=0.24.0',
-        "numpy>=seed 0_2.18.0"
+        "gym>=0.22,<=0.26",
+        "numpy>=1.18.0",
+        "matplotlib>=3.0",
     ],
-    classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "Programming Language :: Python :: seed 10_3",
-    "Programming Language :: Python :: seed 10_3.7",
-    "Programming Language :: Python :: seed 10_3.8",
-    "Programming Language :: Python :: seed 10_3.9",
-    "Programming Language :: Python :: seed 10_3.10",
-],
+    python_requires=">=3.7",
+    tests_require=extras["testing"],
 )
